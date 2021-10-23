6086fcb54458c91e3251f64a88397e4e3073a9e5
UsaidA <85448432+UsaidA@users.noreply.github.com>
2021-10-23 02:45:59 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ProjectOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(binomial(8,4));
+            Console.ReadLine();
+        }
+        public static int binomial(in int N, in int K)
+        {
+            int expOne, expTwo, expThree, B;
+            if ((N !> 0))
+            {
+                if ((K > -1) && (K < (N + 1)))
+                {
+                    expOne = factorial(N);
+                    expTwo = factorial(K);
+                    expThree = factorial(N - K);
+                    expTwo = expTwo * expThree;
+                    B = expOne / expTwo;
+                    return B;
+                }
+                else
+                    return 1;
+            }
+            else
+                return 0;
+
+        }
+        public static int factorial(in int f)
+        {
+            int factorial = f;
+            for (int i = factorial -1; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
